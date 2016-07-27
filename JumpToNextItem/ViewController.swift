cc61f0e63171e9594551a9d6a62968c693042cf0
Fly <imbearfly@efgpdigiwinde-macbook-air.local>
2016-07-27 13:46:30 +0800

    Done

--- JumpToNextItem/ViewController.swift
+++ JumpToNextItem/ViewController.swift
@@ -8,16 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var idTxt: UITextField!
+    @IBOutlet weak var pwdTxt: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pwdTxt.isSecureTextEntry = true
+        idTxt.returnKeyType = .next
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text! == "" {
+            print("A")
+            return false
+        } else if textField == idTxt {
+            textField.resignFirstResponder()
+            pwdTxt.becomeFirstResponder()
+            print("B")
+            return true
+        } else if textField == pwdTxt {
+            print("C")
+            textField.resignFirstResponder()
+            return true
+        }else {
+            print("D")
+            return false
+        }
     }
 
 
